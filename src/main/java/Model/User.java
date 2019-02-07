a5bfa115e6a909079d72ae7ad4e3b3d8c1ed3dbf
Elcbayramov <elcbayramov@gmail.com>
2019-02-08 00:11:36 +0400

    second commit

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,4 +1,25 @@
 package Model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "its_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<UserRole> roles;
 }
--- src/main/java/Repository/UserRepository.java
+++ src/main/java/Repository/UserRepository.java
@@ -1,4 +1,14 @@
 package Repository;
 
-public interface UserRepository {
+
+import Model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findByUsername (String username);
 }
